3dd9c100798eeb6d1a0552dde9976af242aeb824
jabirmahmud24 <jabir.mahmud.saad@gmail.com>
2025-09-04 17:34:53 +0600

    Pronunciation Feature added

--- script/index.js
+++ script/index.js
@@ -2,6 +2,11 @@ const createElements = (arr) => {
   const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
   return htmlElements.join("");
 };
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const manageSpinner = (status) => {
   if (status == true) {
     document.getElementById("spinner").classList.remove("hidden");
@@ -98,7 +103,9 @@ const displayLevelWord = (words) => {
             })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
               <i class="fa-solid fa-circle-info"></i>
             </button>
-            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <button onclick="pronounceWord('${
+              word.word
+            }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
               <i class="fa-solid fa-volume-low"></i>
             </button>
           </div>
@@ -121,3 +128,17 @@ const displayLesson = (lessons) => {
   }
 };
 loadLessons();
+document.getElementById("btn-search").addEventListener("click", () => {
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  console.log(searchValue);
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWords = data.data;
+      const filterWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue)
+      );
+      displayLevelWord(filterWords);
+    });
+});
